1141fed4c85d37b17da59b1402dcb1520900afb5
dellie <delsterone@gmail.com>
2024-05-28 14:27:09 -0500

    got base adding folders and directories working

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use std::error::Error;
 use std::fs;
+use std::fs::read_to_string;
+use std::io::Write;
 use std::process;
+use std::path::Path;
+use std::fs::File;
+use std::io::BufReader;
+use std::io::BufRead;
 
 const CONFIG_FILE_PATH: &str = "./config/webpages.conf";
 
@@ -12,8 +18,8 @@ pub struct Config {
 pub enum ConfigOptions {
     AddFile(String),
     ReadConfig,
-
 }
+
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str>{
         if args.len() < 2 {
@@ -26,47 +32,96 @@ impl Config {
         }
         return Err("Incorrect amount of arguments");
     }
- 
-}
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    match config.config {
-        ConfigOptions::AddFile(file_path) => {
-            add_file(file_path).unwrap_or_else(|err| {
-                println!("Problem parsing file! {err}");
-                process::exit(1);
-            });
-        },
-        ConfigOptions::ReadConfig => {
-            read_config().unwrap_or_else(|err| {
-                println!("Problem parsing file! {err}");
-                process::exit(1);
-            });
-        }
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        let files : Vec<String> = match &self.config {
+            ConfigOptions::AddFile(file_path) => {
+                dbg!(&file_path);
+                add_file(file_path.to_string()).unwrap_or_else(|err| {
+                    dbg!(format!("Problem parsing file! {err}"));
+                    process::exit(1);
+                })
+            },
+            ConfigOptions::ReadConfig => {
+                read_config().unwrap_or_else(|err| {
+                    dbg!("Problem parsing file! {err}");
+                    process::exit(1);
+                })
+            }
+        };
+        serve_files(files);
+        Ok(())
     }
-    Ok(())
 }
-fn add_file(new_file_path : String) -> Result<(), Box<dyn Error>> {
+
+fn add_file(new_file_path : String) -> Result<Vec<String>, Box<dyn Error>> {
     let mut out_string : String = String::from("");
     // refactor this to convert the file into a list of strings, split by newlines (handle indented tabs later)
-    println!("successfully wrote file!");
-    let config_file_contents = fs::read_to_string(&CONFIG_FILE_PATH)?;
-    let mut config_contents = config_file_contents.split('\n').collect::<Vec<&str>>();
-
-    let config_file_contents = fs::read_to_string(&new_file_path)?;
-    config_contents.push(&new_file_path);
-    config_contents.sort();
-    config_contents.dedup();
-    config_contents.into_iter().for_each(|current_file| {
+    let file = File::open(Path::new(&CONFIG_FILE_PATH)).unwrap();
+    let reader = BufReader::new(&file);
+    let mut lines: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
+    lines.remove(0);
+    lines.push(new_file_path.clone());
+    lines.sort();
+    lines.dedup();
+    lines.clone().into_iter().for_each(|current_file| {
 
         out_string += &format!("{current_file}\n");
     });
-    fs::write(&CONFIG_FILE_PATH, out_string);
+    // fs::create_dir("/some/dir")?
+    fs::write(&CONFIG_FILE_PATH, out_string)?;
+    println!("successfully added file!{}", &new_file_path);
+    Ok(lines)
+}
+
+fn serve_files(file_paths: Vec<String>) -> Result<(), Box<dyn Error>> {
+    dbg!(&file_paths);
+    file_paths.iter().for_each(|file_path| {
+        // ERROR: 
+        dbg!(file_path);
+        let file_name = Path::new(&file_path).file_stem().unwrap().to_str().unwrap();
+        
+        // Create the directory path
+        let page_dir_string = format!("./pages/{}", file_name);
+        let page_directory = Path::new(&page_dir_string);
+        
+        // Create the full path to the new file
+        let page_file_string = format!("{}/{}.md", page_dir_string, file_name);
+        let page_file = Path::new(&page_file_string);
+        
+        // Create the directory if it does not exist
+        let _ = fs::create_dir_all(page_directory).unwrap_or_else(|err| println!("Failed to create directory: {}", err));
+        
+        println!("Page Dir: {:?}\nPage File: {:?}", &page_directory, &page_file);
+        
+        
+        // Create the file
+        let curr_file_path_string = String::from(&format!("{file_path}"));
+        dbg!(&curr_file_path_string);
+        let curr_file_path = Path::new(&curr_file_path_string);
+        let curr_file = File::create(page_file).unwrap();
+        let curr_file_contents = fs::read_to_string(curr_file_path).unwrap();
+        fs::write(&page_file, curr_file_contents);
+    });
+
+
     Ok(())
 }
-pub fn read_config() -> Result<(), Box<dyn Error>> {
-    let file_contents = fs::read_to_string(CONFIG_FILE_PATH);
-    let binding = file_contents?;
-    let files = binding.split('\n').collect::<Vec<&str>>();
-    println!("successfully read config, returned {:?}", files);
-    Ok(())
+fn clean_file_path(new_file_path : String) -> String {
+    let mut out : String = String::from("");
+    let count =     new_file_path.matches('/').count();
+
+    if count == 0{ 
+        out = format!("/{}", new_file_path) 
+    }
+    String::from(out)
+}
+pub fn read_config() -> Result<Vec<String>, Box<dyn Error>> {
+    let file_contents = fs::read_to_string(&CONFIG_FILE_PATH)?;
+    let file = File::open(Path::new(&CONFIG_FILE_PATH)).unwrap();
+    let reader = BufReader::new(&file);
+    let mut lines: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
+    lines.remove(0);
+    println!("FOUND CONFIG LINES {:?}", lines);
+    // println!("successfully read config, returned {:?}", lines);
+    Ok(lines.to_vec())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,22 @@ use std::env;
 use std::process;
 use obsidian_site::Config; 
 // TODO: 
-// - refactor lib.rs to match possible ConfigOption Enums
-// - handle adding files to config with this
-
-
+// - Start parsing files to html output when they are in the config directory
+//      - figure out file structure
 fn main() {
+    println!("{}", std::env::current_dir().unwrap().display());
+
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing file! {err}");
+    let mut configuration = Config::build(&args).unwrap_or_else(|err| {
+        dbg!("Problem parsing file! {err}");
         process::exit(1);
     });
     
     // println!(sdfsdfsdf"Searching for {}", config.query);
     // println!("In file {}", config.file_path);
 
-    if let Err(e) = obsidian_site::run(config) {
-        println!("Problem parsing file! {e}");
+    if let Err(e) = configuration.run() {
+        dbg!("Problem parsing file! {e}");
         process::exit(1);
  
     }
